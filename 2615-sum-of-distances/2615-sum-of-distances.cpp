948360a3c030cea3af0dc00b0e46e4a96d9cf020
GAJENDER <90421243+gajender09@users.noreply.github.com>
2026-04-24 02:24:51 +0530

    Time: 43 ms (97.72%), Space: 109.4 MB (92.13%) - LeetHub

--- 2615-sum-of-distances/2615-sum-of-distances.cpp
+++ 2615-sum-of-distances/2615-sum-of-distances.cpp
@@ -5,7 +5,7 @@ public:
         unordered_map<int, vector<int>> mp;
         vector<long long> res(n, 0);
 
-        // store indices of each value
+        // store indices
         for (int i = 0; i < n; i++) {
             mp[nums[i]].push_back(i);
         }
@@ -13,29 +13,24 @@ public:
         // process each group
         for (auto &p : mp) {
             vector<int>& v = p.second;
-            int sz = v.size();
 
-            // prefix sum of indices
-            vector<long long> pref(sz, 0);
-            pref[0] = v[0];
+            long long totalSum = 0;
+            for (int x : v) totalSum += x;
 
-            for (int i = 1; i < sz; i++) {
-                pref[i] = pref[i - 1] + v[i];
-            }
+            long long leftSum = 0, rightSum = totalSum;
+            long long leftCount = 0, rightCount = v.size();
 
-            for (int i = 0; i < sz; i++) {
-                long long left = 0, right = 0;
+            for (int i = 0; i < v.size(); i++) {
+                rightSum -= v[i];
+                rightCount--;
 
-                if (i > 0) {
-                    left = 1LL * i * v[i] - pref[i - 1];
-                }
-
-                if (i < sz - 1) {
-                    right = (pref[sz - 1] - pref[i]) -
-                            1LL * (sz - i - 1) * v[i];
-                }
+                long long left = leftCount * 1LL * v[i] - leftSum;
+                long long right = rightSum - rightCount * 1LL * v[i];
 
                 res[v[i]] = left + right;
+
+                leftSum += v[i];
+                leftCount++;
             }
         }
 
