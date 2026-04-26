633af4749b710070afb2480e357fa2de91a4061c
GAJENDER <90421243+gajender09@users.noreply.github.com>
2026-04-26 19:28:01 +0530

    Time: 102 ms (19.81%), Space: 122.4 MB (17.44%) - LeetHub

--- 2615-sum-of-distances/2615-sum-of-distances.cpp
+++ 2615-sum-of-distances/2615-sum-of-distances.cpp
@@ -1,39 +1,40 @@
 class Solution {
 public:
-    vector<long long> distance(vector<int>& nums) {
+    typedef long long ll;
+
+    vector<ll> distance(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int, vector<int>> mp;
-        vector<long long> res(n, 0);
 
-        // store indices
-        for (int i = 0; i < n; i++) {
-            mp[nums[i]].push_back(i);
+        vector<ll> arr(n, 0);
+
+        unordered_map<int, ll> indexSum;   //nums[i] -> sum of i's
+        unordered_map<int, ll> indexCount; //nums[i] -> frequency
+
+        //Left to Right
+        for(int i = 0; i < n; i++) {
+            ll freq = indexCount[nums[i]];
+            ll sum  = indexSum[nums[i]];
+
+            arr[i] += freq*i - sum;
+
+            indexCount[nums[i]] += 1;
+            indexSum[nums[i]]   += i;
         }
 
-        // process each group
-        for (auto &p : mp) {
-            vector<int>& v = p.second;
+        indexSum.clear();
+        indexCount.clear();
 
-            long long totalSum = 0;
-            for (int x : v) totalSum += x;
+        //Right to Left
+        for(int i = n-1; i >= 0; i--) {
+            ll freq = indexCount[nums[i]];
+            ll sum  = indexSum[nums[i]];
 
-            long long leftSum = 0, rightSum = totalSum;
-            long long leftCount = 0, rightCount = v.size();
+            arr[i] += sum - freq*i;
 
-            for (int i = 0; i < v.size(); i++) {
-                rightSum -= v[i];
-                rightCount--;
-
-                long long left = leftCount * 1LL * v[i] - leftSum;
-                long long right = rightSum - rightCount * 1LL * v[i];
-
-                res[v[i]] = left + right;
-
-                leftSum += v[i];
-                leftCount++;
-            }
+            indexCount[nums[i]] += 1;
+            indexSum[nums[i]]   += i;
         }
 
-        return res;
+        return arr;
     }
 };
