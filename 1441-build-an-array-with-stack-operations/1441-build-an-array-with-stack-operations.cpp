cdeadbd5f362f35d260dc0be44768f6cfca15a66
GAJENDER <90421243+gajender09@users.noreply.github.com>
2026-04-28 14:57:32 +0530

    Time: 0 ms (100%), Space: 10.8 MB (19.79%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     vector<string> buildArray(vector<int>& target, int n) {
-        int m = target.size();
         vector<string> res;
-
         int curr = 1;
 
-        for (int i = 0; i < m; i++) {
-
-            if (target[i] == curr) {
-                res.push_back("Push");
-                curr++;
-            } 
-
-            else {
+        for (int i = 0; i < target.size(); i++) {
+            while (curr < target[i]) {
                 res.push_back("Push");
                 res.push_back("Pop");
                 curr++;
-                i--;
             }
+
+            res.push_back("Push");
+            curr++;
         }
 
         return res;
