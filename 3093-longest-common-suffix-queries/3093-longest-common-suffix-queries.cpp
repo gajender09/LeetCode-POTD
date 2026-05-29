cb910b8c0b2cf1e1d14ae3de4d9eb05fcf21a368
GAJENDER <90421243+gajender09@users.noreply.github.com>
2026-05-29 16:38:45 +0530

    Time: 882 ms (33.24%), Space: 626.7 MB (22.66%) - LeetHub

--- 3093-longest-common-suffix-queries/3093-longest-common-suffix-queries.cpp
+++ 3093-longest-common-suffix-queries/3093-longest-common-suffix-queries.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-
     struct trieNode {
         int idx;
         trieNode* children[26];
@@ -15,7 +14,7 @@ public:
         trieNode* temp = new trieNode();
         temp->idx = i;
 
-        for(int i = 0; i<26; i++) {
+        for (int i = 0; i < 26; i++) {
             temp->children[i] = NULL;
         }
         return temp;
@@ -25,28 +24,28 @@ public:
         string word = wordsContainer[i];
         int n = word.size();
 
-        for(int j = n-1; j >= 0; j--) {
+        for (int j = n - 1; j >= 0; j--) {
             int ch_idx = word[j] - 'a';
 
-            if(pCrawl->children[ch_idx] == NULL) {
+            if (pCrawl->children[ch_idx] == NULL) {
                 pCrawl->children[ch_idx] = getNode(i);
             }
             pCrawl = pCrawl->children[ch_idx];
-            
-            if(wordsContainer[pCrawl->idx].size() > n) {
+
+            if (wordsContainer[pCrawl->idx].size() > n) {
                 pCrawl->idx = i;
             }
         }
     }
 
-    int search(trieNode* pCrawl, string &word) {
+    int search(trieNode* pCrawl, string& word) {
         int result_idx = pCrawl->idx;
         int n = word.size();
 
-        for(int i = n-1; i >= 0; i--) {
-            int ch_idx = word[i]-'a';
+        for (int i = n - 1; i >= 0; i--) {
+            int ch_idx = word[i] - 'a';
             pCrawl = pCrawl->children[ch_idx];
-            if(pCrawl == NULL) {
+            if (pCrawl == NULL) {
                 return result_idx;
             }
             result_idx = pCrawl->idx;
@@ -54,25 +53,26 @@ public:
         return result_idx;
     }
 
-    vector<int> stringIndices(vector<string>& wordsContainer, vector<string>& wordsQuery) {
+    vector<int> stringIndices(vector<string>& wordsContainer,
+                              vector<string>& wordsQuery) {
         int m = wordsContainer.size();
         int n = wordsQuery.size();
         vector<int> result(n);
 
         trieNode* root = getNode(0);
 
-        for(int i = 0 ; i < m; i++) {
-            if(wordsContainer[root->idx].size() > wordsContainer[i].size()) {
+        for (int i = 0; i < m; i++) {
+            if (wordsContainer[root->idx].size() > wordsContainer[i].size()) {
                 root->idx = i;
             }
             insertTrie(root, i, wordsContainer);
         }
 
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             result[i] = search(root, wordsQuery[i]);
         }
 
-        delete root;   // invokes destructor
+        delete root; // invokes destructor
 
         return result;
     }
