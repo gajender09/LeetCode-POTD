a05b3edc344a116e52a3438998760c65fbf45e55
GAJENDER <90421243+gajender09@users.noreply.github.com>
2026-05-31 12:03:43 +0530

    Time: 23 ms (93.28%), Space: 106.8 MB (16.2%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.cpp
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
-        int n = asteroids.size();
-        bool ans = false;
-
         long long m = mass;
 
         sort(begin(asteroids), end(asteroids));
 
-        for (int asteroid : asteroids) {
+        for (int &asteroid : asteroids) {
             if (m >= asteroid) {
                 m += asteroid;
             } 
             else {
-                return ans;
+                return false;
             }
         }
-        ans = true;
 
-        return ans;
+        return true;
     }
 };
