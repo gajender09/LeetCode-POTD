a5d1ed33a66f057ced66eeed3c94ed28b09761e8
GAJENDER <90421243+gajender09@users.noreply.github.com>
2026-07-02 18:08:00 +0530

    Time: 19 ms (60.56%), Space: 38.2 MB (24.35%) - LeetHub

---
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool findSafeWalk(vector<vector<int>>& grid, int health) {
+
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<vector<int>> dir = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+
+        queue<tuple<int, int, int>> q;               // (row, col, remaining health)
+        vector<vector<int>> vis(m, vector<int>(n, -1));
+
+        int startHealth = health - grid[0][0];
+        if (startHealth <= 0) return false;
+
+        q.push({0, 0, startHealth});
+        vis[0][0] = startHealth;
+
+        while (!q.empty()) {
+            auto [r, c, health] = q.front();
+            q.pop();
+
+            if (r == m - 1 && c == n - 1)
+                return true;
+
+            for (auto &v : dir) {
+
+                int newR = r + v[0];
+                int newC = c + v[1];
+
+                if (newR >= 0 && newR < m &&
+                    newC >= 0 && newC < n) {
+
+                    int newHealth = health - grid[newR][newC];
+
+                    if (newHealth > 0 && newHealth > vis[newR][newC]) {
+                        vis[newR][newC] = newHealth;
+                        q.push({newR, newC, newHealth});
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+};
